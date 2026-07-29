079ad8fecdc44c4f097be7be37f7b7d3de78f18d
ian117 <spideramerica71@gmail.com>
2026-07-28 18:28:56 -0600

    feat(user+notifications): Added config module for the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from 'node_modules/@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { Notification } from './entities/notification.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { NotificationsModule } from './notifications/notifications.module';
 
+import { AppController } from './app.controller';
+
+import { AppService } from './app.service';
+
+import { User } from './entities/user.entity';
+import { Notification } from './entities/notification.entity';
+
+import configuration from './config/configuration' 
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'string',
-      password: 'string',
-      database: 'string',
-      entities: [User, Notification],
-      synchronize: false,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        database: configService.get<string>('database.name'),
+        username: configService.get<string>('database.user'),
+        password: configService.get<string>('database.password'),
+        entities: [User, Notification],
+        synchronize: false,
+      })
     }),
     AuthModule,
     NotificationsModule,
